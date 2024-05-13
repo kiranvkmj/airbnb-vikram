7e635ab058b2c9d12180e96f460221bcfa2e4f29
kiran <kiranrajbimla@gmail.com>
2024-05-13 18:44:08 +0530

    third commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,41 @@
 import { headers } from "next/headers";
 import Image from "next/image";
-import { GlobeAltIcon,MenuIcon,UserCircleIcon,UserIcon, SearchIcon  } from '@heroicons/react/solid';
+import { GlobeAltIcon,MenuIcon,UserCircleIcon,UsersIcon, SearchIcon  } from '@heroicons/react/solid';
+import { SetStateAction, useState } from "react";
+
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { DateRangePicker } from 'react-date-range';
 // import {magnifyingGlass} from '@heroicons/react/solid'
+
+
 function Header(){
+    const[SearchInput,setSearchInput]=useState("");
+    const[startDate,setStartDate]=useState(new Date());
+    const[endDate,setEndDate]=useState(new Date());
+    const[noOfGuests,setNoOfGuests]=useState(1);
+
+
+    const  handleSelect= (ranges:any)=>{
+      setStartDate(ranges.selection.startDate);
+      setEndDate(ranges.selection.endDate);
+
+    }
+
+
+    const resetInput=()=>{
+      setSearchInput("")
+    }
+
+    const selectionRange={
+      startDate:startDate,
+      endDate:endDate,
+      key:"selection"
+    }
+
+
+    console.log(SearchInput);
     return(
      <header className="sticky top-0 z-50
      grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -26,7 +59,10 @@ function Header(){
 
 
      <div  className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
-      <input className="flex-grow pl-5 bg-transparent outline-none
+      <input
+      value={SearchInput}
+      onChange={(e)=>setSearchInput(e.target.value)}
+      className="flex-grow pl-5 bg-transparent outline-none
       text-sm text-gray-600 placeholder-gray-400"
       type="text"  
       placeholder="start your search"  />
@@ -50,6 +86,34 @@ function Header(){
       </div>
       </div>
 
+      {SearchInput && (
+        <div className="flex flex-col
+        col-span-3 mx-auto">
+          <DateRangePicker ranges={[selectionRange]}
+          minDate={new Date()}
+          rangeColors={["#FD5B61"]}
+          onChange={handleSelect}
+          />
+
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+
+          <UsersIcon className="h-5" />
+          <input
+           value={noOfGuests}
+           onChange={(event)=>setNoOfGuests(parseInt(event.target.value))}
+           type="number"
+           min={1}
+           className="w-12 pl-2 text-lg outline-none text-red-400"
+           />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow
+            text-gray-500">Cancel</button>
+            <button className="flex-grow text-red-400">Search</button>
+            </div>
+        </div>
+      )}
      
      </header>
     )
---
+++ pages/search.js
@@ -0,0 +1,15 @@
+// -import React from 'react'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Image from "next/image";
+function Search() {
+  return (
+    <div>
+      <Header />
+      <h2>hello i am here</h2>
+      {/* <Footer/> */}
+    </div>
+  )
+}
+
+export default Search
